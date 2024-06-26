e79984fdf00b13ed64f01445b1a45738a89d8f78
Rich Kevan <rskevan@me.com>
2024-06-26 10:28:04 -0700

    Initial Commit

---
+++ README.php
@@ -0,0 +1,26 @@
+👋 Hey there!
+
+I am an experienced python/django & node/typescript fullstack engineer. In a quest to become a better programmer I have started on a journey to add other languages and skills to my toolbox. You can follow my journey on [My Site](https://richkevan.com)
+
+I have done a little with Php/Laravel, but not deep dived into like my current learning journey. This is my first commit of my deep dive to learn this language and add it to my skillset.
+
+In addition to Php/Laravel I am also going to be refreshing/broadening my knowledge of
+1. Node/Typescript
+2. Python/Django 
+
+Along this journey I will also be devoting time to picking up
+1. C#
+2. Flutter
+3. Ruby
+
+I am also in conjuction with the projects I build going to use every opportunity to brushup on the complimentary technologies.
+1. SQL(PostgreSQL, MySQL)
+2. NoSQL(MongoDB)
+3. Redis
+4. Docker
+5. Kubernetes
+
+I hope you will follow me adventures and cheer me on along the way
+
+### My learning journey
+1. [php-hello-world](https://github.com/richkevan/php-learning-journey/tree/main/php-hello-world)
